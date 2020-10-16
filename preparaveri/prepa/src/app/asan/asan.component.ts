6fc2414b0828ed9e39e3777567d5033c759f3bd0
Dario2002 <banfi.dario@iisgalvanimi.edu.it>
2020-10-16 09:50:38 +0000

    fine_prep_ver

--- preparaveri/prepa/src/app/asan/asan.component.ts
+++ preparaveri/prepa/src/app/asan/asan.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';//importare
 import { HttpClient } from '@angular/common/http'; //importare
+import { Asan } from './asan.model';
 
 @Component({
   selector: 'app-asan',
@@ -8,40 +9,40 @@ import { HttpClient } from '@angular/common/http'; //importare
   styleUrls: ['./asan.component.css']
 })
 export class AsanComponent implements OnInit {
-//aggiungo i tre oggetti
-
+  //aggiungo i tre oggetti
+  asanData: Asan[];
+  oAsan: Observable<Asan[]>;
   data: Object; //1
   loading: boolean;//2
-  o :Observable<Object>;//3
+  o: Observable<Object>;//3
 
   constructor(public http: HttpClient) { } //aggiungo nel costruttore la variabile per fare la richiesta http
 
   //aggiungoquesti due metodi (makeRequest e getData)
 
   makeRequest(): void {
-     console.log("here");
-     this.loading = true;
-     this.o = this.http.get('https://3000-f091d9ce-e2ef-4ca0-9083-323a052706c2.ws-eu01.gitpod.io/api/products');
-     this.o.subscribe(this.getData);
-   }
+    console.log("here");
+    this.loading = true;
+    this.o = this.http.get('https://3000-f091d9ce-e2ef-4ca0-9083-323a052706c2.ws-eu01.gitpod.io/api/products');
+    this.o.subscribe(this.getData);
+  }
 
 
-   getData = (d : Object) =>
-   {
-     this.data = d;
-     this.loading = false;
-   }
+  getData = (d: Object) => {
+    this.data = d;
+    this.loading = false;
+  }
 
-   makeCompactRequest(): void {
-     this.loading = true;
-     this.http
-       .get('https://3000-f091d9ce-e2ef-4ca0-9083-323a052706c2.ws-eu01.gitpod.io/api/products')
-       .subscribe(data => {
-       this.data = data;
-       this.loading = false;
-       });
-      }
 
+
+  makeTypedRequest(): void {
+    //oFoo : Observable<Foo[]>; va dichiarato tra gli attributi della classe
+    this.oAsan = this.http.get<Asan[]>('https://3000-f091d9ce-e2ef-4ca0-9083-323a052706c2.ws-eu01.gitpod.io/api/products');
+    this.oAsan.subscribe(data => {
+      this.asanData = data["products"];
+      console.log(this.asanData);
+    });
+  }
 
   ngOnInit(): void {
   }
